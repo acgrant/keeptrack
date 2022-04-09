2f82816eb3f434f8640a1524dce0e3197222e814
Alejandro Carrizosa Grant <alejandrocarrizosagrant@Alejandros-MBP.home>
2022-04-08 22:31:41 -0700

    [pb-415]: base scraper functionality working

--- src/scraper/pageScraper.d.ts
+++ src/scraper/pageScraper.d.ts
@@ -1,9 +1,44 @@
 const scraperObject = {
-    url: '',
+    url: 'https://randompatientgenerator.netlify.app/',
     async scraper (browser) {
-        let page = await browser.newPage();
         console.log(`Navigating to ${this.url}`);
+        let page = await browser.newPage();
         await page.goto(this.url);
+        await page.waitForSelector('a.button');
+        await page.waitForSelector('#age');
+        await page.click('a.button');
+        const stats = await page.evaluate(() => {
+            let patients = [];
+            document.querySelectorAll('p2')
+                .forEach((element, idx) => {
+                    patients.push({
+                        idx: element.textContent
+                    })
+                })
+
+            //
+            // age: '#age',
+            //     sex: '#sex',
+            //     chief_complaint: '#cc',
+            //     heart_rate: '#hr',
+            //     temperature: '#temp',
+            //     respiratory_rate: '#rr',
+            //     oxygen_saturation: '#sat',
+            //     history_1: '#pmh1',
+            //     history_2: '#pmh2',
+            // const patient = {};
+            // patient['age'] = document.getElementById('#age');
+            // patient['sex'] = document.getElementById('#sex');
+            // patient['chief_complaint'] = document.getElementById('#cc');
+            // patient['heart_rate'] = document.getElementById('#hr');
+            // patient['temperature'] = document.getElementById('#temp');
+            // patient['respiratory_rate'] = document.getElementByIdr('#rr');
+            // patient['oxygen_saturation'] = document.getElementById('#sat');
+            // patient['history_1'] = document.getElementById('#pmh1');
+            // patient['history_2'] = document.getElementById('#pmh2');
+            return patients;
+        })
+        console.log(stats)
     }
 }
 
