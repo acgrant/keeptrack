9a93accab29261ad96c9c4d45dda33d5ea72355d
Alejandro Carrizosa Grant <alejandrocarrizosagrant@Alejandros-MBP.home>
2022-04-11 00:56:31 -0700

    [pb-415]: able to write to file in project directory but still need to work on getting the content outside of evaluate method on puppeteer

--- src/scraper/pageScraper.d.ts
+++ src/scraper/pageScraper.d.ts
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const scraperObject = {
     url: 'https://randompatientgenerator.netlify.app/',
     async scraper (browser) {
@@ -6,30 +8,36 @@ const scraperObject = {
         await page.goto(this.url);
         await page.waitForSelector('a.button');
         await page.waitForSelector('#age');
-        await page.click('a.button');
-        const stats = await page.evaluate(() => {
-            const patients = [];
-            const patient = {}
-            document.querySelectorAll('p2')
-                .forEach((element, idx) => {
-                    switch (idx) {
-                        case 1: patient['age'] = element.textContent;
-                        case 2: patient['sex'] = element.textContent;
-                        case 3: patient['chief_complaint'] = element.textContent;
-                        case 4: patient['heart_rate'] = element.textContent;
-                        case 5: patient['blood_pressure'] = element.textContent;
-                        case 6: patient['temperature'] = element.textContent;
-                        case 7: patient['respiratory_rate'] = element.textContent;
-                        case 8: patient['oxygen_saturation'] = element.textContent;
-                        case 9: patient['history_1'] = element.textContent;
-                        case 10: patient['history_2'] = element.textContent;
-                        default: console.log('Patient added.')
-                    }
-                })
-            patients.push(patient)
-            return patients;
-        })
-        console.log(stats)
+        let patientCount = 0;
+        while (patientCount < 10) {
+            await page.click('a.button');
+            await page.evaluate(() => {
+                const patients = [];
+                const patient = {};
+                document.querySelectorAll('p2')
+                    .forEach((element, idx) => {
+                        switch (idx) {
+                            case 1: patient['age'] = element.textContent;
+                            case 2: patient['sex'] = element.textContent;
+                            case 3: patient['chief_complaint'] = element.textContent;
+                            case 4: patient['heart_rate'] = element.textContent;
+                            case 5: patient['blood_pressure'] = element.textContent;
+                            case 6: patient['temperature'] = element.textContent;
+                            case 7: patient['respiratory_rate'] = element.textContent;
+                            case 8: patient['oxygen_saturation'] = element.textContent;
+                            case 9: patient['history_1'] = element.textContent;
+                            case 10: patient['history_2'] = element.textContent;
+                            default: console.log('Patient added.')
+                        }
+                    });
+                patients.push(patient);
+            });
+            patientCount++;
+        }
+        fs.writeFile('../projects/MockProjects.json', JSON.stringify({'I': 'exist'}, null, 4), (err) => {
+            if (err) throw err;
+            else console.log('Data successfully appended.');
+        });
     }
 }
 
